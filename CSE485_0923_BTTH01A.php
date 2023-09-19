8180a7a87a9dc4c6776a793283c954bb38947087
nguyendacson <131184812+nguyendacson@users.noreply.github.com>
2023-09-19 09:22:01 +0700

    CSE485_0923_BTTH01A.php

--- CSE485_0923_BTTH01A.php
+++ CSE485_0923_BTTH01A.php
@@ -1,16 +1,20 @@
+<!-- Ý 1  -->
 <?php
 $arr = [2, 5, 6, 9, 2, 5, 6, 12, 5];
 
 $tong = 0;
-$tich = 0;
+$tich = 1;
 $hieu = $arr[0];
 $thuong = $arr[0]; 
 
 for ($i = 0; $i < count($arr); $i++) {
     $tong += $arr[$i];
     $tich *= $arr[$i];
-    $hieu -= $arr[$i];
-    $thuong /= $arr[$i];
+    if ($i != 0) 
+    {
+        $hieu -= $arr[$i];
+        $thuong /= $arr[$i];
+    }
 }
 
 
@@ -26,59 +30,92 @@ echo 'Thương các phần tử: ' . $TinhThuong .'<br>'. '<br>';
 
 ?>
 
+<!-- Ý 2 -->
+
+
+<!-- Ý 3 -->
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title> ý 2</title>
-    <style>
-        .red
-        {
-            color: red;
-        }
-
-        .blue
-        {
-            color: blue;
-        }
-
-        .orange
-        {
-            color:orange;
-        }
-
-        .white
-        {
-            color: white;
-        }
-    </style>
+    <title>Bảng HTML trong PHP</title>
 </head>
 <body>
     <?php
-        $arrs = ['đỏ', 'xanh', 'cam', 'trắng'];
-        foreach ($arrs as $color) 
-        {
-            if ($color === 'đỏ') {
-                echo "<span class='red'>$color</span><br>";
-            } 
-            else if ($color === 'xanh') 
-            {
-                echo "<span class='blue'>$color</span><br>";
-            }
-            else if ($color === 'cam')
-            {
-                echo "<span class='orange'>$color</span><br>";
-            }
-            else if ($color === 'trắng')
-            {
-                echo "<span class='white'>$color</span><br>";
+    $data = [
+        ['<strong>Tên khóa học </strong>'],
+        ['PHP'],
+        ['HTML'],
+        ['CSS'],
+        ['JS']
+    ];
 
-                echo  " Màu đỏ là màu yêu thích của Anh, trắng là màu yêu thích của Sơn, cam là màu yêu 
-                thích của Thắng, còn màu yêu thích của tôi là màu trắng " ;
-            }
-        } 
+    echo '<table border="1">';
+    foreach ($data as $row) {
+        echo '<tr>';
+        foreach ($row as $cell) {
+            echo '<td>' . $cell . '</td>';
+        }
+        echo '</tr>';
+    }
+    echo '</table>';
     ?>
 </body>
 </html>
 
+<!--Ý 4 -->
+<?php
+$arrs = array(
+    "Italy" => "Rome", 
+    "Luxembourg" => "Luxembourg", 
+    "Belgium" => "Brussels", 
+    "Denmark" => "Copenhagen", 
+    "Finland" => "Helsinki", 
+    "France" => "Paris", 
+    "Slovakia" => "Bratislava", 
+    "Slovenia" => "Ljubljana", 
+    "Germany" => "Berlin", 
+    "Greece" => "Athens", 
+    "Ireland" => "Dublin", 
+    "Netherlands" => "Amsterdam", 
+    "Portugal" => "Lisbon", 
+    "Spain" => "Madrid", 
+    "Sweden" => "Stockholm", 
+    "United Kingdom" => "London", 
+    "Cyprus" => "Nicosia", 
+    "Lithuania" => "Vilnius", 
+    "Czech Republic" => "Prague", 
+    "Estonia" => "Tallin", 
+    "Hungary" => "Budapest", 
+    "Latvia" => "Riga", 
+    "Malta" => "Valetta", 
+    "Austria" => "Vienna", 
+    "Poland" => "Warsaw"
+);
+foreach ($arrs as $country => $capital) {
+    echo "Thủ đô của $country là $capital <br>";}
+?>
+
+<!-- Ý 5 -->
+<?php 
+    echo '<br>';
+
+    $a = 
+    [
+        'a' => ['b' => 0,'c' => 1],
+        'b' => ['e' => 2,'o' => ['b' => 3]]
+    ];
+
+    $b3 = $a['b']['o']['b'];
+    echo 'giá trị = 3 có key là b = ' .$b3 .'<br>';
+    $a1 = $a['a']['c'];
+    echo 'giá trị = 1 mà có key là c = ' . $a1 .'<br>';
+ 
+    $ab = $a['a']['b'];
+    $abc = $a['a']['c'];
+    echo 'thông tin của phần tử có key là a là : ' . $ab . ' ' . $abc;
+   ;
+
+?>
+<!-- Ý 6 -->
